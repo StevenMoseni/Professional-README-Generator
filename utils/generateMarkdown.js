91aa0dd35f9a644b274ef5b1a264afb2983ffc9a
Steven Moseni <smoseni@hotmail.com>
2021-03-09 23:03:15 -0800

    generate Mardown.js file

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,36 @@
+function generateMarkdown(data) {
+  return `
+# Project Title
+${data.title}
+${data.getLicense}
+
+# Description
+${data.description}
+
+# Table of Contents 
+* [Installation](#-Installation)
+* [Usage](#-Usage)
+* [License](#-Installation)
+* [Contributing](#-Contributing)
+* [Tests](#-Tests)
+* [Questions](#-Contact-Information)
+  
+# Installation
+${data.installation}
+# Usage
+${data.usage}
+# License 
+${data.license}
+# Contributing 
+${data.contributing}
+# Tests
+${data.tests}
+
+# Contact Information 
+* Contact Email: ${data.userEmail}
+* GitHub Username: ${data.userName}
+
+`;
+}
+
+module.exports = generateMarkdown;
